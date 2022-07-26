be4a510e2481f516b0dbada26c211f3b5029a1c8
Marcos Alves <zuckeer@gmail.com>
2022-07-25 22:22:33 -0300

    Uso da tag Link do next

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
-import { useState } from "react"
+import Link from 'next/link'
 
 function Home() {
     return <div>
         <h1>Página de teste inicial</h1>
+        
+        <Link href="/sobre">
+            <a>Acessar página Sobre</a>
+        </Link>
+        
     </div>
 }
 
---
+++ pages/sobre.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+function Sobre() {
+    return <div>
+        <h1>Página Sobre</h1>
+        
+        <Link href="/">
+            <a>Acessar página Home</a>
+        </Link>
+        
+    </div>
+}
+
+export default Sobre
